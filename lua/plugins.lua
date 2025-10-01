f8d53f23042ab1b838c02c314c9762a606ccda1f
Takaaki-Inaba <inaba@valinux.co.jp>
2025-10-01 21:50:37 +0900

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -109,36 +109,6 @@ require("lazy").setup({
 		branch = "main"
 	},
 	{"FabijanZulj/blame.nvim"},
-	-- {
-	-- 	"nvim-treesitter/nvim-treesitter",
-	-- 	build = ":TSUpdate",
-	-- 	config = function ()
-	-- 		local configs = require("nvim-treesitter.configs")
-	--
-	-- 		configs.setup({
-	-- 			ensure_installed = {
-	-- 				"c",
-	-- 				"lua",
-	-- 				"vim",
-	-- 				"vimdoc",
-	-- 				"query",
-	-- 				"go",
-	-- 				"bash",
-	-- 				"cpp",
-	-- 				"html",
-	-- 				"cmake",
-	-- 				"make",
-	-- 				"markdown",
-	-- 				"python",
-	-- 				"yaml",
-	-- 				"toml"
-	-- 			},
-	-- 			sync_install = false,
-	-- 			highlight = { enable = true },
-	-- 			indent = { enable = true },
-	-- 		})
-	-- 	end
-	-- },
 })
 
 vim.cmd("let g:lightline = { 'colorscheme': 'moonfly' }")
